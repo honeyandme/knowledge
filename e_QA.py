480bde37348d43eb41af83bb70875c0cc5bb3b1c
honeyandme <2414342539@qq.com>
2023-04-14 10:45:49 +0800

    better QA

--- e_QA.py
+++ e_QA.py
@@ -1,5 +1,4 @@
 import random
-
 import d_Ner as zwk
 from transformers import BertModel,BertTokenizer
 import torch
@@ -25,8 +24,10 @@ def get_pre_tool():
 class QuestionCls():
     def __init__(self,client):
         self.client = client
-        self.ask_dru = ['吃什么药','什么药','哪种药','啥药']
-        self.ask_cure = ['怎么办','治疗']
+        self.ask_dru_list = ['什么药','哪种药','啥药']
+        self.ask_cure_list = ['怎么办','治疗']
+        self.ask_dis_sympotm_list = ['症状','表现','怎么样']
+        self.ask_food_list = []
         self.hello = ['您好！','你好！','你好,我是聊天机器人！','您好呀！']
     def ask_cure_method(self,name):
         query1 = "match (a:疾病{名称:'%s'})-[r:治疗的方法]->(b:治疗方法) return b.名称" % (name)
@@ -47,12 +48,36 @@ class QuestionCls():
             return ""
         res = res[0]['a.名称']
         return res
+    def ask_dis_symptom(self,name):
+        query = "match (a:疾病{名称:'%s'})-[r:疾病的症状]->(b:疾病症状) return b.名称" % (name)
+        res = self.client.run(query).data()
+        res = [d['b.名称'] for d in res]
+        if len(res) > 0:
+            out = f'如果得了{name}，可能会有如下症状:' + ";".join(res)
+        else:
+            out = "抱歉！无法获取您想知道的信息。"
+        if out[-1] != '。':
+            out = out + '。'
+        return out
+    def ask_dru(self,name):
+        query = "match (a:疾病{名称:'%s'})-[r:疾病使用药品]->(b:药品) return b.名称" % (name)
+        res = self.client.run(query).data()
+        res = [d['b.名称'] for d in res]
+        if len(res) > 0:
+            out = f'如果得了{name}，可以尝试这些药品:' + ";".join(res)
+        else:
+            out = "抱歉！无法获取您想知道的信息。"
+        if out[-1] != '。':
+            out = out + '。'
+        return out
+    def keyword_in(self,sen,keyword):
+        for word in keyword:
+            if word in sen:
+                return True
+        return False
     def ask(self,sen,en_list):
-        is_ask_cure = False
-        for t in self.ask_cure:
-            if t in sen:
-                is_ask_cure = True
-        if is_ask_cure:
+        is_ask_dru_list = self.keyword_in(sen,self.ask_cure_list)
+        if is_ask_dru_list:
             if "疾病" in en_list:
                 return random.choice(self.hello) + self.ask_cure_method(en_list.get("疾病",""))
             elif "疾病症状" in en_list:
@@ -62,6 +87,24 @@ class QuestionCls():
                 out = random.choice(self.hello)+'我推测您可能是得了{%s}'%(dis_name) +'。'
                 out += self.ask_cure_method(dis_name)
                 return out
+
+        is_ask_dis_symptom = self.keyword_in(sen,self.ask_dis_sympotm_list)
+        if is_ask_dis_symptom:
+            if "疾病" in en_list:
+                return self.ask_dis_symptom(en_list["疾病"])
+
+        is_ask_dru = self.keyword_in(sen,self.ask_dru_list)
+        if is_ask_dru:
+            if "疾病" in en_list:
+                return random.choice(self.hello)+self.ask_dru(en_list.get("疾病",""))
+            elif "疾病症状" in en_list:
+                dis_name = self.ask_dis_name(en_list.get("疾病症状",""))
+                if len(dis_name)==0:
+                    return random.choice(self.hello) + '我不知道您为什么会'+en_list.get("疾病症状","")
+                out = random.choice(self.hello)+'我推测您可能是得了{%s}'%(dis_name) +'。'
+                out += self.ask_dru(dis_name)
+                return out
+        return "抱歉，我无法识别您的问题～"
 
 
 
